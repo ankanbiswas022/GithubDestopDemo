73c45e68ec8ec6365ef3d15714c698a56b98cf67
Ankan Biswas <ankanbiswas022@gmail.com>
2022-04-14 16:47:05 +0530

    Added the sum functionality
    - We have added lines so that given two numbers you would get sum as output

--- FirstScript.m
+++ FirstScript.m
@@ -1,3 +1,10 @@
 % hello world
 
 disp('hello world');
+
+%giving out put of two numbers
+
+x=input('Give me the first number: ');
+y=input('Give me the second number: ');
+z=x+y;
+disp(['The sum of both the number is = ' num2str(z)]);
